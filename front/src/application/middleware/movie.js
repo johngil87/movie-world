89c1f2a722ff58400d41e8724ded7d82d7452949
diego alejandro vélez <vegodiego@hotmail.com>
2021-07-29 02:45:48 -0500

    se implemento la funcionalidad eliminar pelicula favorita en la capa application

--- front/src/application/middleware/movie.js
+++ front/src/application/middleware/movie.js
@@ -1,71 +1,85 @@
-import {GET_MOVIES_WITHOUT_FILTER, GET_FAVORITE_MOVIES, GET_MOVIES_BY_CATEGORY, GET_MOVIES_BY_TITLE, GET_TOP_MOVIES, setMovieListSuccess, setMovieListFailure, setTopMovieListSuccess, setTopMovieListFailure} from "../actions/movie";
+import * as actions from '../actions/movie';
 import { transformMovieListFromBack } from "./helper/helper";
 
 
 const getMoviesWithoutFilterFlow = ({api}) => ({dispatch}) => next => async (action) => {
     next(action);
-    if(action.type === GET_MOVIES_WITHOUT_FILTER){
+    if(action.type === actions.GET_MOVIES_WITHOUT_FILTER){
         try{
             const movieListfromBack = await api.movie.getAll();
             const movieList = transformMovieListFromBack(movieListfromBack);
-            dispatch(setMovieListSuccess(movieList));
+            dispatch(actions.setMovieListSuccess(movieList));
         }catch (error){
-            dispatch(setMovieListFailure(error.message));
+            dispatch(actions.setMovieListFailure(error.message));
         }
     }
 }
 
 const getTopMoviesFlow = ({api}) => ({dispatch}) => next => async (action) => {
     next(action);
-    if(action.type === GET_TOP_MOVIES){
+    if(action.type === actions.GET_TOP_MOVIES){
         try{
             const movieListfromBack = await api.movie.getTop();
             const movieList = transformMovieListFromBack(movieListfromBack);
-            dispatch(setTopMovieListSuccess(movieList));
+            dispatch(actions.setTopMovieListSuccess(movieList));
         }catch (error){
-            dispatch(setTopMovieListFailure(error.message));
+            dispatch(actions.setTopMovieListFailure(error.message));
         }
     }
 }
 
 const getMoviesByTitleFlow = ({api}) => ({dispatch}) => next => async (action) => {
     next(action);
-    if(action.type === GET_MOVIES_BY_TITLE){
+    if(action.type === actions.GET_MOVIES_BY_TITLE){
         try{
             const title = action.payload.title;
             const movieListfromBack = await api.movie.getByTitle(title);
             const movieList = transformMovieListFromBack(movieListfromBack);
-            dispatch(setMovieListSuccess(movieList));
+            dispatch(actions.setMovieListSuccess(movieList));
         }catch (error){
-            dispatch(setMovieListFailure(error.message));
+            dispatch(actions.setMovieListFailure(error.message));
         }
     }
 }
 
 const getMoviesByCategoryFlow = ({api}) => ({dispatch}) => next => async (action) => {
     next(action);
-    if(action.type === GET_MOVIES_BY_CATEGORY){
+    if(action.type === actions.GET_MOVIES_BY_CATEGORY){
         try{
             const category = action.payload.category;
             const movieListfromBack = await api.movie.getByCategory(category);
             const movieList = transformMovieListFromBack(movieListfromBack);
-            dispatch(setMovieListSuccess(movieList));
+            dispatch(actions.setMovieListSuccess(movieList));
         }catch (error){
-            dispatch(setMovieListFailure(error.message));
+            dispatch(actions.setMovieListFailure(error.message));
         }
     }
 }
 
 const getFavoriteMoviesFlow = ({api}) => ({dispatch}) => next => async (action) => {
     next(action);
-    if(action.type === GET_FAVORITE_MOVIES){
+    if(action.type === actions.GET_FAVORITE_MOVIES){
         try{
             const userId = action.payload
             const movieListfromBack = await api.movie.getFavorites(userId);
             const movieList = transformMovieListFromBack(movieListfromBack);
-            dispatch(setMovieListSuccess(movieList));
+            dispatch(actions.setMovieListSuccess(movieList));
         }catch (error){
-            dispatch(setMovieListFailure(error.message));
+            dispatch(actions.setMovieListFailure(error.message));
+        }
+    }
+}
+
+const deleteFavoriteMovieFlow = ({api}) => ({dispatch}) => next => async (action) => {
+    next(action);
+    if(action.type === actions.DELETE_FAVORITE_MOVIE){
+        try{
+            const userId = action.payload.userId;
+            const movieId = action.payload.movieId;
+            await api.movie.deleteFavorite(userId, movieId);
+            dispatch(actions.deleteFavoriteMovieSuccess());
+        }catch (error){
+            dispatch(actions.deleteFavoriteMovieFailure(error.message));
         }
     }
 }
@@ -76,5 +90,6 @@ export default [
     getTopMoviesFlow,
     getMoviesByTitleFlow,
     getMoviesByCategoryFlow,
-    getFavoriteMoviesFlow
+    getFavoriteMoviesFlow,
+    deleteFavoriteMovieFlow
 ]
